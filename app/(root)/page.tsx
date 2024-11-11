81c12d9de5227cdb01b1616156e123b8043304f1
IzePhanthakarn <phanthakarn@soilfish.co>
2024-11-12 01:37:55 +0700

    🔨 set sanity into project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -6,16 +9,40 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  return (
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch Your Startup, <br /> Connect With Entrepreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
-      </p>
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-      <SearchForm query={query} />
-    </section>
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="noresults">No startup found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
+    </>
   );
 }
